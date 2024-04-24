8fb5359ea758c055a765e3ad01e1f7c9e10970cb
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-24 12:17:48 +0200

    add navbar

---
+++ src/components/nav/nav.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+const Navbar = () => {
+  return (
+    <nav className="flex gap-10 p-5 border-b-sky-950 border-b-2 rounded-b-md shadow-lg bg-sky-200">
+      <ul>
+        <li className="bg-">
+          <a href=""></a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
