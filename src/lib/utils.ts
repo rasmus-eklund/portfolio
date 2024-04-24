4a1050ef4ea2463a09f58dbc39e9f96ae4da5e83
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-24 14:51:31 +0200

    Add scroll to

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,16 +1,16 @@
-import { type ClassValue, clsx } from 'clsx';
-import { Dispatch, RefObject, SetStateAction } from 'react';
-import { twMerge } from 'tailwind-merge';
+import { type ClassValue, clsx } from "clsx";
+import { Dispatch, RefObject, SetStateAction } from "react";
+import { twMerge } from "tailwind-merge";
 
 export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs));
 
 export const scrollTo = (
   top: number,
   ref: RefObject<HTMLElement>,
-  setScroll: Dispatch<SetStateAction<number>>
+  setScroll: Dispatch<SetStateAction<number>>,
 ) => {
   if (ref.current) {
     setScroll(0);
-    ref.current.scrollTo({ top, behavior: 'smooth' });
+    ref.current.scrollTo({ top, behavior: "smooth" });
   }
 };
