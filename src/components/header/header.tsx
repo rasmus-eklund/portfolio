1d4978c462bf44f66605337a76e9dabda7350be9
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-24 14:51:57 +0200

    add header

---
+++ src/components/header/header.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+const Header = () => {
+  return (
+    <header className="flex flex-col items-center gap-4">
+      <Image
+        className="size-48 rounded-full border-[1px] border-sky-800"
+        src="https://avatars.githubusercontent.com/u/49008491?v=4"
+        alt="Photo of Rasmus Eklund"
+        height={256}
+        width={256}
+        priority
+      />
+      <h1 className="text-xl text-sky-950">
+        Hello and welcome to my portfolio!
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
