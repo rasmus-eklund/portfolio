3db08dcf49ba5ae80e2c3a1f408f6a1c0eed7e9b
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-24 12:18:00 +0200

    add footer

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,13 @@
+const footer = () => {
+  return (
+    <footer className="flex justify-between items-center border-t-2 rounded-t-md border-t-sky-900 bg-sky-400 px-2 py-1 text-sky-950 font-bold">
+      <span>&copy; Rasmus Eklund</span>
+      <div className="flex gap-2 items-center">
+        <a href="https://www.linkedin.com/in/rasmus-eklund/">LinkedIn</a>
+        <a href="https://github.com/rasmus-eklund">GitHub</a>
+      </div>
+    </footer>
+  );
+};
+
+export default footer;
