c3817e1eb899badfaba31391eb5163b4f0e20e97
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-25 12:45:15 +0200

    Fix open in new tab

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,10 +1,22 @@
 const footer = () => {
   return (
-    <footer className="w-full sticky bottom-0 flex justify-between items-center border-t rounded-t-md border-t-sky-950/50 bg-sky-400 px-2 py-1 text-sky-950 font-bold">
-      <span>&copy; Rasmus Eklund</span>
-      <div className="flex gap-2 items-center">
-        <a href="https://www.linkedin.com/in/rasmus-eklund/">LinkedIn</a>
-        <a href="https://github.com/rasmus-eklund">GitHub</a>
+    <footer className="sticky bottom-0 flex w-full items-center justify-between rounded-t-md border-t border-t-sky-950/50 bg-sky-400 px-2 py-1 font-bold">
+      <span className="text-sky-950">&copy; Rasmus Eklund</span>
+      <div className="flex items-center gap-2">
+        <a
+          className="text-sky-950"
+          href="https://www.linkedin.com/in/rasmus-eklund/"
+          target="_blank"
+        >
+          LinkedIn
+        </a>
+        <a
+          className="text-sky-950"
+          href="https://github.com/rasmus-eklund"
+          target="_blank"
+        >
+          GitHub
+        </a>
       </div>
     </footer>
   );
