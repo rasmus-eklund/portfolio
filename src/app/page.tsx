626dbfae48c24435430f0ddd70f19714e914bd4a
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-25 12:43:05 +0200

    Reset url on to top. Fix padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,15 +6,17 @@ import Projects from "@/components/projects/projects";
 import { Button } from "@/components/ui/button";
 import useScrollPosition from "@/hooks/useScrollPosition";
 import { scrollTo } from "@/lib/utils";
+import { useRouter } from "next/navigation";
 import { useRef } from "react";
 
 const Home = () => {
+  const router = useRouter();
   const main = useRef<HTMLElement>(null);
   const [scroll, setScroll] = useScrollPosition(main);
   return (
     <main
       ref={main}
-      className="no-scrollbar relative flex w-full max-w-5xl grow flex-col items-center gap-6 overflow-y-auto scroll-smooth px-2 py-16 md:gap-16"
+      className="no-scrollbar relative flex w-full max-w-5xl grow flex-col items-center gap-6 overflow-y-auto scroll-smooth px-2 pb-16 md:gap-16"
     >
       <Header />
       <Separator />
@@ -23,7 +25,10 @@ const Home = () => {
       <Projects />
 
       <Button
-        onClick={() => scrollTo(0, main, setScroll)}
+        onClick={() => {
+          router.push("/");
+          scrollTo(0, main, setScroll);
+        }}
         className={`fixed bottom-10 right-4 w-fit transition-opacity duration-300 ${
           scroll > 200 ? "opacity-100" : "hidden opacity-0"
         }`}
