1aba1775bf1938fe888909ff9c70afda1138c223
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-25 07:03:12 +0200

    add projects data

---
+++ src/components/projects/projectsData.ts
@@ -0,0 +1,57 @@
+export const projectsData = [
+  {
+    title: "MatPlan",
+    github: "https://github.com/rasmus-eklund/MatPlanT3",
+    href: "https://matplan.dev",
+    description:
+      "MatPlan is a versatile recipe app in Swedish that goes beyond the traditional recipe management. It allows you to plan your meals for the week, create shopping lists, and optimize your grocery shopping. Whether you're a cooking enthusiast or just trying to simplify your meal planning, MatPlan has got you covered!",
+    tech: [
+      "Next.js",
+      "React",
+      "React Query",
+      "Tailwind",
+      "tRPC",
+      "Prisma",
+      "PostgreSQL",
+      "Vercel",
+      "Git+GitHub",
+      "Supabase",
+      "Meilisearch",
+    ],
+  },
+  {
+    title: "RICA Banken",
+    github: "https://github.com/rasmus-eklund/Budget",
+    href: "https://pengar.vercel.app/",
+    description:
+      "At RICA Banken, you can upload your ICA Banken transactions using password encryption. You can upload transactions from multiple people and their respective accounts. The appliation will figure out transactions that are internal between the accounts. Keep track of you monthly income and expenses grouped by the categories you provide. Take a look at the demo page if you don't have data to upload.",
+    tech: [
+      "Next.js",
+      "React",
+      "Tailwind",
+      "NextAuth",
+      "Drizzle",
+      "PostgreSQL",
+      "Supabase",
+      "Vercel",
+      "Git+GitHub",
+    ],
+  },
+  {
+    title: "OnChainBet",
+    github: "https://github.com/rasmus-eklund/OnChainBet",
+    href: "https://onchainbet.vercel.app/",
+    description:
+      "OnChainBet, an Ethereum-based betting application built with Solidity smart contracts. OnChainBet allows you to place bets on the Ethereum Sepolia testnet, oﬀering both live and demo modes.",
+    tech: [
+      "Next.js",
+      "React",
+      "Tailwind",
+      "hardhat",
+      "web3.js",
+      "ethers.js",
+      "Vercel",
+      "Git+GitHub",
+    ],
+  },
+];
