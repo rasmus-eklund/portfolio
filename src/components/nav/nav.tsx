d12e9adde590359f9ec54b7ca88cdcac40944a09
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-25 12:54:06 +0200

    Different nav depending on media

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -1,13 +1,16 @@
 "use client";
 
+import Desktop from "./desktop";
+import Phone from "./phone";
+
 const Navbar = () => {
   return (
-    <nav className="sticky top-0 flex w-full gap-10 rounded-b-md border-b-2 border-b-sky-950 bg-sky-200 p-5 shadow-lg z-10">
-      <ul>
-        <li className="bg-">
-          <a href=""></a>
-        </li>
-      </ul>
+    <nav className="sticky top-0 z-10 flex w-full justify-between gap-10 rounded-b-md border-b border-b-sky-950 bg-sky-200 p-5 shadow-lg">
+      <h1 className="text-nowrap text-sky-950 text-xl">
+        <a href="#start">Rasmus Eklund</a>
+      </h1>
+      <Desktop />
+      <Phone />
     </nav>
   );
 };
