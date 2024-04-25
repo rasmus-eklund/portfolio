67d6ebbf26d7038436c8b9ba109187b2a774b5d9
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-25 07:01:27 +0200

    Add icon component

---
+++ src/assets/icons.tsx
@@ -0,0 +1,36 @@
+import type { RefObject, SVGProps } from "react";
+import { cn } from "../lib/utils";
+
+const svgPaths = {
+  plus: "M12 4.5v15m7.5-7.5h-15",
+  minus: "M5 12h14",
+  webpage:
+    "M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418",
+  code: "m6.75 7.5 3 2.25-3 2.25m4.5 0h3m-9 8.25h13.5A2.25 2.25 0 0 0 21 18V6a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 6v12a2.25 2.25 0 0 0 2.25 2.25Z",
+  education:
+    "M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5",
+} as const;
+
+export type IconType = keyof typeof svgPaths;
+type Props = SVGProps<SVGElement> & {
+  ref?: RefObject<SVGSVGElement>;
+  icon: IconType;
+};
+
+const Icon = ({ icon, className, ...rest }: Props) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      className={cn("size-6", className)}
+      strokeWidth={1.5}
+      fill="none"
+      stroke="currentColor"
+      {...rest}
+    >
+      <path strokeLinecap="round" strokeLinejoin="round" d={svgPaths[icon]} />
+    </svg>
+  );
+};
+
+export default Icon;
